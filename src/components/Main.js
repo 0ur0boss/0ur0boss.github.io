1415ae7d24842521837267a089e4e28c6311cdf5
0ur0boss <enzo.duret@yahoo.fr>
2022-02-04 23:10:12 +0100

    ajoue des svgR et du power btn (retour a l accueil)

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import styled from 'styled-components'
+import PowerButton from '../subComponents/PowerButton';
+
+
+const MainContainer = styled.div`
+/* permet de recuperer le theme du body */
+background: ${props => props.theme.body};
+width: 100vw;
+height: 100vh;
+overflow:hidden;
+position: relative;
+
+h2,h3,h4,h5,h6{
+  font-family: 'Karla', sans-serif;
+  font-weight: 500;
+}
+`
+const Container = styled.div`
+padding: 2rem;
+`
 
 const Main = () => {
-  return <div>
-      Main Component/Page
-  </div>;
+  return <MainContainer>
+      <Container>
+       <PowerButton></PowerButton>
+      </Container>
+  </MainContainer>;
 };
 
 export default Main;
